973672f85f052d3790029f153e0b684a9d8bfd7a
Marcin Szupke <marcin.szupke@gmail.com>
2020-04-28 20:27:04 +0200

    stworzenie inicjalnego projektu Spring Boot

---
+++ src/test/java/pl/sda/todopp/TodoppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.todopp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
